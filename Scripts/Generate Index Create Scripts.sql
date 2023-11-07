2aa03821b4be4ee655a02f021b317456dd64762e
Chad Baldwin <chad@chadbaldwin.net>
2023-11-06 20:21:50 -0500

    Update Generate Index Create Scripts.sql

--- Scripts/Generate Index Create Scripts.sql
+++ Scripts/Generate Index Create Scripts.sql
@@ -1,8 +1,9 @@
-SELECT n.SchemaName, n.ObjectName, n.IndexName, IndexDefinition = CONCAT_WS(' '
-		, 'CREATE', IIF(i.is_unique = 1, 'UNIQUE', NULL), i.[type_desc] COLLATE DATABASE_DEFAULT, 'INDEX', QUOTENAME(n.IndexName)
-		, 'ON', CONCAT(QUOTENAME(n.SchemaName),'.', QUOTENAME(n.ObjectName))
+SELECT n.SchemaName, n.ObjectName, n.IndexName
+	, IndexDefinition = CONCAT_WS(' '
+		, 'CREATE', IIF(i.is_unique = 1, 'UNIQUE', NULL), i.[type_desc] COLLATE DATABASE_DEFAULT, 'INDEX', qn.IndexName
+		, 'ON', qn.SchemaName+'.'+qn.ObjectName
 		, '('+kc.KeyCols+')', 'INCLUDE ('+kc.InclCols+')'
-		, 'WHERE ' + i.filter_definition, 'WITH ('+x.Options+')'
+		, 'WHERE '+i.filter_definition, 'WITH ('+x.Options+')'
 		, IIF(ds.is_default = 0, 'ON '+QUOTENAME(ds.[name]), NULL)
 	--	, 'FILESTREAM_ON {{FilestreamGroup|PartitionName|"NULL"}}'
 	)+';'
@@ -12,6 +13,7 @@ FROM sys.indexes i
 	JOIN sys.partitions p ON p.[object_id] = i.[object_id] AND p.index_id = i.index_id AND p.partition_number = 1 -- Partitioning not yet supported
 	JOIN sys.data_spaces ds ON ds.data_space_id = i.data_space_id
 	CROSS APPLY (SELECT SchemaName = SCHEMA_NAME(o.[schema_id]), ObjectName = o.[name], IndexName = i.[name]) n
+	CROSS APPLY (SELECT SchemaName = QUOTENAME(n.SchemaName), ObjectName = QUOTENAME(n.ObjectName), IndexName = QUOTENAME(n.IndexName)) qn
 	CROSS APPLY (
 		SELECT KeyCols = STRING_AGG(IIF(ic.is_included_column = 0, x.Col, NULL), ', ')
 			, InclCols = STRING_AGG(IIF(ic.is_included_column = 1, x.Col, NULL), ', ')
@@ -23,13 +25,13 @@ FROM sys.indexes i
 		SELECT Options = STRING_AGG(CONCAT(opt.n, '=', opt.v), ', ')
 		FROM (
 			VALUES ('PAD_INDEX'				, IIF(i.is_padded = 1			, 'ON', NULL))
-				,  ('FILLFACTOR'			, IIF(i.fill_factor > 0			, CONVERT(varchar(3), i.fill_factor), NULL))
+				,  ('FILLFACTOR'			, CONVERT(varchar(3), NULLIF(i.fill_factor, 0)))
 				,  ('IGNORE_DUP_KEY'		, IIF(i.[ignore_dup_key] = 1	, 'ON', NULL))
 				,  ('STATISTICS_NORECOMPUTE', IIF(st.no_recompute = 1		, 'ON', NULL))
 				,  ('STATISTICS_INCREMENTAL', IIF(st.is_incremental = 1		, 'ON', NULL))
 				,  ('ALLOW_ROW_LOCKS'		, IIF(i.[allow_row_locks] = 1	, NULL, 'OFF'))
 				,  ('ALLOW_PAGE_LOCKS'		, IIF(i.[allow_page_locks] = 1	, NULL, 'OFF'))
-				,  ('DATA_COMPRESSION'		, NULLIF(p.[data_compression_desc] COLLATE DATABASE_DEFAULT, 'NONE'))
+				,  ('DATA_COMPRESSION'		, NULLIF(p.[data_compression_desc] COLLATE DATABASE_DEFAULT, 'NONE')) -- Only works for non-partitioned tables
 			--	,  ('XML_COMPRESSION'		, NULL) -- Haven't figured it out yet
 				,  ('ONLINE'				, IIF(SERVERPROPERTY('EngineEdition') = 3, 'ON', NULL)) -- 3 = Eval/Dev/Enterprise
 		) opt(n,v)
