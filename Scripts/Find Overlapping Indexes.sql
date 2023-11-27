8ad89a045f417886e66946b37e59a954ae85a810
Chad Baldwin <chad@chadbaldwin.net>
2023-11-26 22:06:24 -0500

    Update Find Overlapping Indexes.sql

--- Scripts/Find Overlapping Indexes.sql
+++ Scripts/Find Overlapping Indexes.sql
@@ -1,3 +1,9 @@
+/*	Needs to be `bigint` for reasons...
+	decimal(38,0) is implicitly converted to float when getting the value of POWER(@2, N)
+	With float, you lose precision...So 4611686018427387904 (2^62) becomes 4611686018427387900
+	because it wants to treat it as 4.61168601842739E+18.
+
+	However, if you use bigint...then it always treats it as a bigint and no precision is lost. */
 DECLARE @2 bigint = 2;
 
 IF OBJECT_ID('tempdb..#tmp_idx','U') IS NOT NULL DROP TABLE #tmp_idx; --SELECT * FROM #tmp_idx
@@ -13,8 +19,9 @@ FROM sys.indexes i
 	CROSS APPLY (
 		SELECT KeyCols     = STRING_AGG(IIF(ic.is_included_column = 0, CONCAT(IIF(ic.is_descending_key = 1, '-', ''), ic.column_id), NULL), ',') WITHIN GROUP (ORDER BY ic.key_ordinal)+','
 			,  InclCols    = STRING_AGG(IIF(ic.is_included_column = 1, ic.column_id, NULL), ',') WITHIN GROUP (ORDER BY ic.key_ordinal)+','
-			-- Only supports tables with up to 186 columns....after that you're gonna have to edit the query yourself
-			-- Magic number '63' - bigint is 2^63-1 - so 63-1 is the highest we can go. Someone please explain to me why binary/varbinary datatypes don't support bitwise operators.
+			/*	Only supports tables with up to 189 columns....after that you're gonna have to edit the query yourself, just follow the pattern.
+				Magic number '63' - bigint is 8 bytes (64 bits). The 64th bit is used for signing (+/-), so the highest bit we can use is the 63rd bit position.
+				Also, someone please explain to me why bitwise operators don't support binary/varbinary on both sides? */
 			,  InclBitmap1 = CONVERT(bigint, SUM(IIF(ic.is_included_column = 1 AND (ic.column_id > 63*0 AND ic.column_id <= 63*1), POWER(@2, ic.column_id-1 - 63*0), 0))) --   0 < column_id <=  63
 			,  InclBitmap2 = CONVERT(bigint, SUM(IIF(ic.is_included_column = 1 AND (ic.column_id > 63*1 AND ic.column_id <= 63*2), POWER(@2, ic.column_id-1 - 63*1), 0))) --  63 < column_id <= 126
 			,  InclBitmap3 = CONVERT(bigint, SUM(IIF(ic.is_included_column = 1 AND (ic.column_id > 63*2 AND ic.column_id <= 63*3), POWER(@2, ic.column_id-1 - 63*2), 0))) -- 126 < column_id <= 189
@@ -42,8 +49,19 @@ FROM #tmp_idx x
 		SELECT i.IndexName, i.KeyCols, i.InclCols, i.InclBitmap1, i.InclBitmap2
 		FROM #tmp_idx i
 		WHERE i.[object_id] = x.[object_id] AND i.index_id <> x.index_id
+			-- We only want to match indexes like for like - unique to unique, filtered to filtered, etc.
 			AND i.is_unique = x.is_unique AND i.has_filter = x.has_filter AND i.filter_definition = x.filter_definition
+			-- Right wildcard match. Key columns are ordinal and sort dependent - `A,B,-C,D,` matches to `A,B,-C,` but not `A,-C,B,D,` and not `A,-B,C,`
 			AND x.KeyCols LIKE i.KeyCols + '%'
+			/*
+				If  the source bitmap is 10110 (indicating columns 2, 3, 5)
+				And the target bitmap is 10100 (indicating columns 3, 5)
+				Bitwise 'and'(&) returns 10100. Since that matches the target bitmap, we know the source bitmap contains all columns.
+
+				If  the source bitmap is 10110 (indicating columns 2, 3, 5)
+				And the target bitmap is 10101 (indicating columns 1, 3, 5)
+				Bitwise 'and'(&) returns 10100. This does not match the target bitmap, so we know it is not fully covering (missing column 1).
+			*/
 			AND i.InclBitmap1 & x.InclBitmap1 = i.InclBitmap1
 			AND i.InclBitmap2 & x.InclBitmap2 = i.InclBitmap2
 			AND i.InclBitmap3 & x.InclBitmap3 = i.InclBitmap3
