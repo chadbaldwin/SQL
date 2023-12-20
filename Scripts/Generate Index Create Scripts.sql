1285b0b98c3ee48be22c808e228387c444979e98
Chad Baldwin <cbaldwin@hchb.com>
2023-12-19 22:58:36 -0500

    Because unicode everything

--- Scripts/Generate Index Create Scripts.sql
+++ Scripts/Generate Index Create Scripts.sql
@@ -10,7 +10,7 @@
 ------------------------------------------------------------------------------
 -- Populate temp table with dependent infomation
 ------------------------------------------------------------------------------
-IF OBJECT_ID('tempdb..#tmp_indexes','U') IS NOT NULL DROP TABLE #tmp_indexes; --SELECT * FROM #tmp_indexes
+IF OBJECT_ID(N'tempdb..#tmp_indexes',N'U') IS NOT NULL DROP TABLE #tmp_indexes; --SELECT * FROM #tmp_indexes
 SELECT ObjectID             = i.[object_id]
     , IndexID               = i.index_id
     , SchemaName            = SCHEMA_NAME(o.[schema_id])
@@ -41,13 +41,13 @@ FROM sys.indexes i
     LEFT HASH JOIN sys.partitions p ON p.[object_id] = i.[object_id] AND p.index_id = i.index_id AND p.partition_number = 1 -- Partitioning not yet supported
     JOIN sys.data_spaces ds ON ds.data_space_id = i.data_space_id
     CROSS APPLY (
-        SELECT KeyColsN      = STRING_AGG(IIF(ic.is_included_column = 0, n.ColName          , NULL), '{{delim}}') WITHIN GROUP (ORDER BY ic.key_ordinal, ic.index_column_id)
-            ,  KeyColsNQO    = STRING_AGG(IIF(ic.is_included_column = 0, t.ColNameQuoteOrder, NULL), '{{delim}}') WITHIN GROUP (ORDER BY ic.key_ordinal, ic.index_column_id)
-            ,  InclColsNQ    = STRING_AGG(IIF(ic.is_included_column = 1, q.ColNameQuote     , NULL), '{{delim}}') WITHIN GROUP (ORDER BY ic.key_ordinal, ic.index_column_id)
+        SELECT KeyColsN      = STRING_AGG(IIF(ic.is_included_column = 0, n.ColName          , NULL), N'{{delim}}') WITHIN GROUP (ORDER BY ic.key_ordinal, ic.index_column_id)
+            ,  KeyColsNQO    = STRING_AGG(IIF(ic.is_included_column = 0, t.ColNameQuoteOrder, NULL), N'{{delim}}') WITHIN GROUP (ORDER BY ic.key_ordinal, ic.index_column_id)
+            ,  InclColsNQ    = STRING_AGG(IIF(ic.is_included_column = 1, q.ColNameQuote     , NULL), N'{{delim}}') WITHIN GROUP (ORDER BY ic.key_ordinal, ic.index_column_id)
         FROM sys.index_columns ic
             CROSS APPLY (SELECT ColName = COL_NAME(ic.[object_id], ic.column_id)) n
             CROSS APPLY (SELECT ColNameQuote = QUOTENAME(n.ColName)) q
-            CROSS APPLY (SELECT ColNameQuoteOrder = CONCAT_WS(' ', q.ColNameQuote, IIF(ic.is_descending_key = 1, 'DESC', NULL))) t
+            CROSS APPLY (SELECT ColNameQuoteOrder = CONCAT_WS(N' ', q.ColNameQuote, IIF(ic.is_descending_key = 1, N'DESC', NULL))) t
         WHERE ic.[object_id] = i.[object_id] AND ic.index_id = i.index_id
     ) kc
 WHERE i.[type] > 0 -- Exclude heaps
@@ -58,98 +58,100 @@ WHERE i.[type] > 0 -- Exclude heaps
 
 ------------------------------------------------------------------------------
 -- Options
-DECLARE @ScriptIfNotExists  bit           = 1,
-        @ScriptIfExists     bit           = 1,
-        @EnableOnline       bit           = IIF(SERVERPROPERTY('EngineEdition') = 3, 1, 0),
-        @BatchSeparator     bit           = 1,
-        @FormatSQL          bit           = 1,
-        @TrailingLineBreak  bit           = 1,
-        @AddOutputMessages  bit           = 1,
-        @MAXDOP             tinyint       = 0; -- 0 = Default
+DECLARE @ScriptIfNotExists  bit            = 1,
+        @ScriptIfExists     bit            = 1,
+        @EnableOnline       bit            = IIF(SERVERPROPERTY('EngineEdition') = 3, 1, 0),
+        @BatchSeparator     bit            = 1,
+        @FormatSQL          bit            = 1,
+        @TrailingLineBreak  bit            = 1,
+        @AddOutputMessages  bit            = 1,
+        @MAXDOP             tinyint        = 0; -- 0 = Default
 -- Other
-DECLARE @crlf               char(2)       = CHAR(13)+CHAR(10),
-        @tab                char(1)       = CHAR(9);
+DECLARE @crlf               nchar(2)       = NCHAR(13)+NCHAR(10),
+        @tab                nchar(1)       = NCHAR(9);
 -- Templates
-DECLARE @SqlIfNotExists     nvarchar(MAX) = N'IF (INDEXPROPERTY(OBJECT_ID(''{{Schema}}.{{Object}}''), ''{{Index}}'', ''IndexId'') IS NULL)',
-        @SqlIfExists        nvarchar(MAX) = N'IF (INDEXPROPERTY(OBJECT_ID(''{{Schema}}.{{Object}}''), ''{{Index}}'', ''IndexId'') IS NOT NULL)',
-        @SqlDrop            nvarchar(MAX) = N'DROP INDEX IF EXISTS {{Index}} ON {{Schema}}.{{Object}};',
-        @SqlRebuild         nvarchar(MAX) = N'ALTER INDEX {{Index}} ON {{Schema}}.{{Object}} REBUILD',
-        @SqlDisable         nvarchar(MAX) = N'ALTER INDEX {{Index}} ON {{Schema}}.{{Object}} DISABLE;',
-        @SqlOutputMessage   nvarchar(MAX) = N'RAISERROR(''Execute: {{Message}}'',0,1) WITH NOWAIT;',
-        @SqlErrorMessage    nvarchar(MAX) = N'RAISERROR(''ERROR: {{Message}}'',11,1) WITH NOWAIT;';
+DECLARE @SqlIfNotExists     nvarchar(4000) = N'IF (INDEXPROPERTY(OBJECT_ID(N''{{Schema}}.{{Object}}''), N''{{Index}}'', ''IndexId'') IS NULL)',
+        @SqlIfExists        nvarchar(4000) = N'IF (INDEXPROPERTY(OBJECT_ID(N''{{Schema}}.{{Object}}''), N''{{Index}}'', ''IndexId'') IS NOT NULL)',
+        @SqlDrop            nvarchar(4000) = N'DROP INDEX IF EXISTS {{Index}} ON {{Schema}}.{{Object}};',
+        @SqlRebuild         nvarchar(4000) = N'ALTER INDEX {{Index}} ON {{Schema}}.{{Object}} REBUILD',
+        @SqlDisable         nvarchar(4000) = N'ALTER INDEX {{Index}} ON {{Schema}}.{{Object}} DISABLE;',
+        @SqlOutputMessage   nvarchar(4000) = N'RAISERROR(''Execute: {{Message}}'',0,1) WITH NOWAIT;',
+        @SqlErrorMessage    nvarchar(4000) = N'RAISERROR(''ERROR: {{Message}}'',11,1) WITH NOWAIT;';
 ------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------
 SELECT i.SchemaName, i.ObjectName, i.IndexName, i.ObjectType, i.IndexType, i.IsDisabled, i.HasFilter
-    , KeyCols = REPLACE(i.KeyColsNQO , '{{delim}}',', '), InclCols = REPLACE(i.InclColsNQ, '{{delim}}',', ')
-    , c.SuggestedName, MatchesSuggestedName = CONVERT(bit, IIF(i.IndexName = c.SuggestedName, 1, 0))
-    , CreateScript  = REPLACE(REPLACE(y.IfNotExists, '{{Message}}', REPLACE(c.CreateBase      ,'''','''''')), '{{Script}}', z.CompleteCreate)
-    , DropScript    = REPLACE(REPLACE(y.IfExists   , '{{Message}}', REPLACE(s.DropScript      ,'''','''''')), '{{Script}}', s.DropScript)
-    , RebuildScript = REPLACE(REPLACE(y.IfExists   , '{{Message}}', REPLACE(s.RebuildScript   ,'''','''''')), '{{Script}}', CONCAT_WS(' ', s.RebuildScript, c.BuildOptions) + ';')
-    , DisableScript = REPLACE(REPLACE(y.IfExists   , '{{Message}}', REPLACE(s.DisableScript   ,'''','''''')), '{{Script}}', s.DisableScript)
-    , VerifyDrop    = s.IfExists + @crlf + 'BEGIN;' + @crlf + @tab + REPLACE(@SqlErrorMessage , '{{Message}}', REPLACE(s.DropScript   ,'''','''''')) + @crlf + 'END;' + c.BatchSeparator
+    , KeyCols              = REPLACE(i.KeyColsNQO , N'{{delim}}',N', ')
+	, InclCols             = REPLACE(i.InclColsNQ, N'{{delim}}',N', ')
+    , SuggestedName        = c.SuggestedName
+	, MatchesSuggestedName = CONVERT(bit, IIF(i.IndexName = c.SuggestedName, 1, 0))
+    , CreateScript         = REPLACE(REPLACE(y.IfNotExists, N'{{Message}}', REPLACE(c.CreateBase   ,N'''',N'''''')), N'{{Script}}', z.CompleteCreate)
+    , DropScript           = REPLACE(REPLACE(y.IfExists   , N'{{Message}}', REPLACE(s.DropScript   ,N'''',N'''''')), N'{{Script}}', s.DropScript)
+    , RebuildScript        = REPLACE(REPLACE(y.IfExists   , N'{{Message}}', REPLACE(s.RebuildScript,N'''',N'''''')), N'{{Script}}', CONCAT_WS(N' ', s.RebuildScript, c.BuildOptions) + N';')
+    , DisableScript        = REPLACE(REPLACE(y.IfExists   , N'{{Message}}', REPLACE(s.DisableScript,N'''',N'''''')), N'{{Script}}', s.DisableScript)
+    , VerifyDrop           = s.IfExists + @crlf + N'BEGIN;' + @crlf + @tab + REPLACE(@SqlErrorMessage , N'{{Message}}', REPLACE(s.DropScript   ,N'''',N'''''')) + @crlf + N'END;' + c.BatchSeparator
 FROM #tmp_indexes i
     CROSS APPLY (SELECT SchemaName = QUOTENAME(i.SchemaName), ObjectName = QUOTENAME(i.ObjectName), IndexName = QUOTENAME(i.IndexName)) qn
     -- Create the base scripts for each section
     CROSS APPLY (
-        SELECT IfNotExists   = REPLACE(REPLACE(REPLACE(@SqlIfNotExists, '{{Schema}}', qn.SchemaName), '{{Object}}', qn.ObjectName), '{{Index}}', i.IndexName)
-            ,  IfExists      = REPLACE(REPLACE(REPLACE(@SqlIfExists   , '{{Schema}}', qn.SchemaName), '{{Object}}', qn.ObjectName), '{{Index}}', i.IndexName)
-            ,  DisableScript = REPLACE(REPLACE(REPLACE(@SqlDisable    , '{{Schema}}', qn.SchemaName), '{{Object}}', qn.ObjectName), '{{Index}}', qn.IndexName)
-            ,  RebuildScript = REPLACE(REPLACE(REPLACE(@SqlRebuild    , '{{Schema}}', qn.SchemaName), '{{Object}}', qn.ObjectName), '{{Index}}', qn.IndexName)
-            ,  DropScript    = REPLACE(REPLACE(REPLACE(@SqlDrop       , '{{Schema}}', qn.SchemaName), '{{Object}}', qn.ObjectName), '{{Index}}', qn.IndexName)
+        SELECT IfNotExists   = REPLACE(REPLACE(REPLACE(@SqlIfNotExists, N'{{Schema}}', qn.SchemaName), N'{{Object}}', qn.ObjectName), N'{{Index}}', i.IndexName)
+            ,  IfExists      = REPLACE(REPLACE(REPLACE(@SqlIfExists   , N'{{Schema}}', qn.SchemaName), N'{{Object}}', qn.ObjectName), N'{{Index}}', i.IndexName)
+            ,  DisableScript = REPLACE(REPLACE(REPLACE(@SqlDisable    , N'{{Schema}}', qn.SchemaName), N'{{Object}}', qn.ObjectName), N'{{Index}}', qn.IndexName)
+            ,  RebuildScript = REPLACE(REPLACE(REPLACE(@SqlRebuild    , N'{{Schema}}', qn.SchemaName), N'{{Object}}', qn.ObjectName), N'{{Index}}', qn.IndexName)
+            ,  DropScript    = REPLACE(REPLACE(REPLACE(@SqlDrop       , N'{{Schema}}', qn.SchemaName), N'{{Object}}', qn.ObjectName), N'{{Index}}', qn.IndexName)
     ) s
     CROSS APPLY (
-        SELECT CreateOptions = STRING_AGG(IIF(opt.IsBuildOption = 0, CONCAT(opt.n, '=', opt.v), NULL), ', ')
-            ,  BuildOptions  = STRING_AGG(IIF(opt.IsBuildOption = 1, CONCAT(opt.n, '=', opt.v), NULL), ', ')
+        SELECT CreateOptions = STRING_AGG(IIF(opt.IsBuildOption = 0, CONCAT(opt.n, N'=', opt.v), NULL), N', ')
+            ,  BuildOptions  = STRING_AGG(IIF(opt.IsBuildOption = 1, CONCAT(opt.n, N'=', opt.v), NULL), N', ')
         FROM (
-            VALUES (0, 'PAD_INDEX'             , IIF(i.IsPadded = 1            , 'ON', NULL))
-                ,  (0, 'FILLFACTOR'            , CONVERT(varchar(3), NULLIF(i.[FillFactor], 0)))
-                ,  (0, 'IGNORE_DUP_KEY'        , IIF(i.IgnoreDupKey = 1        , 'ON', NULL))
-                ,  (0, 'STATISTICS_NORECOMPUTE', IIF(i.StatNoRecompute = 1     , 'ON', NULL))
-                ,  (0, 'STATISTICS_INCREMENTAL', IIF(i.StatIsIncremental = 1   , 'ON', NULL))
-                ,  (0, 'ALLOW_ROW_LOCKS'       , IIF(i.AllowRowLocks = 1       , NULL, 'OFF'))
-                ,  (0, 'ALLOW_PAGE_LOCKS'      , IIF(i.AllowPageLocks = 1      , NULL, 'OFF'))
-                ,  (0, 'DATA_COMPRESSION'      , NULLIF(i.DataCompressionType, 'NONE')) -- Only works for non-partitioned tables
-                ,  (0, 'XML_COMPRESSION'       , NULL) -- Haven't figured it out yet
+            VALUES (0, N'PAD_INDEX'             , IIF(i.IsPadded = 1            , N'ON',   NULL))
+                ,  (0, N'FILLFACTOR'            , CONVERT(nvarchar(3), NULLIF(i.[FillFactor], 0)))
+                ,  (0, N'IGNORE_DUP_KEY'        , IIF(i.IgnoreDupKey = 1        , N'ON',   NULL))
+                ,  (0, N'STATISTICS_NORECOMPUTE', IIF(i.StatNoRecompute = 1     , N'ON',   NULL))
+                ,  (0, N'STATISTICS_INCREMENTAL', IIF(i.StatIsIncremental = 1   , N'ON',   NULL))
+                ,  (0, N'ALLOW_ROW_LOCKS'       , IIF(i.AllowRowLocks = 1       , NULL , N'OFF'))
+                ,  (0, N'ALLOW_PAGE_LOCKS'      , IIF(i.AllowPageLocks = 1      , NULL , N'OFF'))
+                ,  (0, N'DATA_COMPRESSION'      , NULLIF(i.DataCompressionType, N'NONE')) -- Only works for non-partitioned tables
+                ,  (0, N'XML_COMPRESSION'       , NULL) -- Haven't figured it out yet
                 -- Create options
-                ,  (1, 'ONLINE'                , IIF(@EnableOnline = 1         , 'ON', NULL)) -- 3 = Eval/Dev/Enterprise
-                ,  (1, 'MAXDOP'                , CONVERT(varchar(3), NULLIF(@MAXDOP, 0)))
+                ,  (1, N'ONLINE'                , IIF(@EnableOnline = 1         , N'ON',   NULL)) -- 3 = Eval/Dev/Enterprise
+                ,  (1, N'MAXDOP'                , CONVERT(nvarchar(3), NULLIF(@MAXDOP, 0)))
         ) opt(IsBuildOption, n,v)
         WHERE opt.v IS NOT NULL -- Exclude default values
     ) x
     CROSS APPLY (
-        SELECT CreateBase     = CONCAT_WS(' ', 'CREATE', IIF(i.IsUnique = 1, 'UNIQUE', NULL), i.IndexType, 'INDEX', qn.IndexName)   -- CREATE UNIQUE INDEX [IX_TableName]
-            ,  CreateOn       = CONCAT_WS(' ', 'ON', qn.SchemaName+'.'+qn.ObjectName)                                               -- ON [dbo].[TableName]
-            ,  Cols           = '('+REPLACE(i.KeyColsNQO,'{{delim}}',', ')+')'                                                      -- ([KeyCol1], [KeyCol2], [KeyCol3])
-            ,  InclCols       = 'INCLUDE ('+REPLACE(i.InclColsNQ,'{{delim}}',', ')+')'                                              -- INCLUDE ([ColA], [ColB], [ColC])
-            ,  Filtered       = 'WHERE '+i.FilterDefinition                                                                         -- WHERE ([ColA] = 123)
-            ,  CreateOptions  = 'WITH ('+NULLIF(CONCAT_WS(', ', x.CreateOptions, x.BuildOptions),'')+')'                            -- WITH (PAD_INDEX=ON, FILLFACTOR=85, ONLINE=ON)
-            ,  DataSpace      = 'ON '+IIF(i.IndexFGIsDefault = 0, QUOTENAME(i.IndexFGName), NULL)                                   -- ON [Secondary]
+        SELECT CreateBase     = CONCAT_WS(N' ', N'CREATE', IIF(i.IsUnique = 1, N'UNIQUE', NULL), i.IndexType, N'INDEX', qn.IndexName) -- CREATE UNIQUE INDEX [IX_TableName]
+            ,  CreateOn       = CONCAT_WS(N' ', N'ON', qn.SchemaName+N'.'+qn.ObjectName)                                              -- ON [dbo].[TableName]
+            ,  Cols           = N'('+REPLACE(i.KeyColsNQO,N'{{delim}}',N', ')+N')'                                                    -- ([KeyCol1], [KeyCol2], [KeyCol3])
+            ,  InclCols       = N'INCLUDE ('+REPLACE(i.InclColsNQ,N'{{delim}}',N', ')+N')'                                            -- INCLUDE ([ColA], [ColB], [ColC])
+            ,  Filtered       = N'WHERE '+i.FilterDefinition                                                                          -- WHERE ([ColA] = 123)
+            ,  CreateOptions  = N'WITH ('+NULLIF(CONCAT_WS(N', ', x.CreateOptions, x.BuildOptions),N'')+N')'                          -- WITH (PAD_INDEX=ON, FILLFACTOR=85, ONLINE=ON)
+            ,  DataSpace      = N'ON '+IIF(i.IndexFGIsDefault = 0, QUOTENAME(i.IndexFGName), NULL)                                    -- ON [Secondary]
 
-            ,  BuildOptions   = 'WITH ('+x.BuildOptions+')'
-            ,  BatchSeparator = IIF(@BatchSeparator = 1, @crlf + 'GO', '') + IIF(@TrailingLineBreak = 1, @crlf+@crlf, '')
-            ,  SuggestedName  = CONCAT('IX_', i.ObjectName, '_', REPLACE(i.KeyColsN,'{{delim}}','_'))
+            ,  BuildOptions   = N'WITH ('+x.BuildOptions+N')'
+            ,  BatchSeparator = IIF(@BatchSeparator = 1, @crlf + N'GO', N'') + IIF(@TrailingLineBreak = 1, @crlf+@crlf, N'')
+            ,  SuggestedName  = LEFT(CONCAT(N'IX_', i.ObjectName, N'_', REPLACE(i.KeyColsN,N'{{delim}}',N'_')), 128)
     ) c
     CROSS APPLY (
-        SELECT IfExists    = IIF(@ScriptIfExists    = 1, s.IfExists    + @crlf + 'BEGIN;' + @crlf, '')
-                           + IIF(@AddOutputMessages = 1, IIF(@ScriptIfExists    = 1, @tab, '') + @SqlOutputMessage + @crlf, '')
-                           + IIF(@ScriptIfExists    = 1, @tab, '') + '{{Script}}'
-                           + IIF(@ScriptIfExists    = 1, @crlf + 'END;', '')
+        SELECT IfExists    = IIF(@ScriptIfExists    = 1, s.IfExists    + @crlf + N'BEGIN;' + @crlf, N'')
+                           + IIF(@AddOutputMessages = 1, IIF(@ScriptIfExists    = 1, @tab, N'') + @SqlOutputMessage + @crlf, N'')
+                           + IIF(@ScriptIfExists    = 1, @tab, N'') + N'{{Script}}'
+                           + IIF(@ScriptIfExists    = 1, @crlf + N'END;', N'')
                            + c.BatchSeparator
 
-            ,  IfNotExists = IIF(@ScriptIfNotExists = 1, s.IfNotExists + @crlf + 'BEGIN;' + @crlf, '')
-                           + IIF(@AddOutputMessages = 1, IIF(@ScriptIfNotExists = 1, @tab, '') + @SqlOutputMessage + @crlf, '')
-                           + IIF(@ScriptIfNotExists = 1, @tab, '') + '{{Script}}'
-                           + IIF(@ScriptIfNotExists = 1, @crlf + 'END;', '')
+            ,  IfNotExists = IIF(@ScriptIfNotExists = 1, s.IfNotExists + @crlf + N'BEGIN;' + @crlf, N'')
+                           + IIF(@AddOutputMessages = 1, IIF(@ScriptIfNotExists = 1, @tab, N'') + @SqlOutputMessage + @crlf, N'')
+                           + IIF(@ScriptIfNotExists = 1, @tab, N'') + N'{{Script}}'
+                           + IIF(@ScriptIfNotExists = 1, @crlf + N'END;', N'')
                            + c.BatchSeparator
     ) y
     CROSS APPLY (
         SELECT CompleteCreate = CONCAT(c.CreateBase
-                                , IIF(@FormatSQL = 1, @crlf + IIF(@ScriptIfNotExists = 1, @tab+@tab, @tab), ' ') + c.CreateOn , ' ' , c.Cols
-                                , IIF(@FormatSQL = 1, @crlf + IIF(@ScriptIfNotExists = 1, @tab+@tab, @tab), ' ') + c.InclCols
-                                , IIF(@FormatSQL = 1, @crlf + IIF(@ScriptIfNotExists = 1, @tab+@tab, @tab), ' ') + c.Filtered
-                                , IIF(@FormatSQL = 1, @crlf + IIF(@ScriptIfNotExists = 1, @tab+@tab, @tab), ' ') + c.CreateOptions
-                                , IIF(@FormatSQL = 1, @crlf + IIF(@ScriptIfNotExists = 1, @tab+@tab, @tab), ' ') + c.DataSpace
-                                , ';')
+                                , IIF(@FormatSQL = 1, @crlf + IIF(@ScriptIfNotExists = 1, @tab+@tab, @tab), N' ') + c.CreateOn , N' ' , c.Cols
+                                , IIF(@FormatSQL = 1, @crlf + IIF(@ScriptIfNotExists = 1, @tab+@tab, @tab), N' ') + c.InclCols
+                                , IIF(@FormatSQL = 1, @crlf + IIF(@ScriptIfNotExists = 1, @tab+@tab, @tab), N' ') + c.Filtered
+                                , IIF(@FormatSQL = 1, @crlf + IIF(@ScriptIfNotExists = 1, @tab+@tab, @tab), N' ') + c.CreateOptions
+                                , IIF(@FormatSQL = 1, @crlf + IIF(@ScriptIfNotExists = 1, @tab+@tab, @tab), N' ') + c.DataSpace
+                                , N';')
     ) z
 ORDER BY i.SchemaName, i.ObjectName, i.IndexName;
