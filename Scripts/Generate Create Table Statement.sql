b481e0732103d74e225f37b0d2d991eb0e41c0fa
Chad Baldwin <cbaldwin@hchb.com>
2024-01-15 15:26:25 -0500

    Add to list of reserved keywords

--- Scripts/Generate Create Table Statement.sql
+++ Scripts/Generate Create Table Statement.sql
@@ -49,13 +49,18 @@ SELECT @output = (
 			) dt
 			CROSS APPLY (
 				SELECT ColName = IIF(x.[name] IN ( -- Add sqare brackets only to reserved keywords or names containing unescaped characters
-										'action','address','application','data','date','day','dayofyear','definition','deny','description','enabled','endpoint',
-										'expiredate','filename','hash','index','level','location','message','minutes','month','monthname','name','or','order',
-										'password','path','population','port','priority','provider','quarter','rank','read','replace','role','row','rowcount',
-										'sequence','server','source','state','status','subject','text','trim','type','uid','url','user','value','version','weekday',
-										'year','object_id','schema_id','type_desc','lock_escalation','durability','messages','weight','ignore_dup_key','allow_row_locks',
-										'allow_page_locks','compression_delay','partition_id'
-									) OR x.[name] LIKE '%[^a-z0-9_#]%' OR x.[name] LIKE '[0-9]%', QUOTENAME(x.[name]), x.[name])
+						'action','address','affinity','algorithm','allow_encrypted_value_modifications','allow_page_locks','allow_row_locks','ansi_defaults','ansi_null_dflt_on','ansi_nulls','ansi_padding','ansi_warnings','app_name','application','arithabort','automated_backup_preference',
+						'availability_mode','backup_priority','broker_instance','bucket_count','catalog','clear_port','cluster_type','columns','compatibility_level','compression_delay','concat_null_yields_null','context_info','conversation_group_id','conversation_handle','counter','data',
+						'data_compression','data_source','database','database_name','database_principal_id','date','date_format','datefirst','dateformat','day','dayofyear','days','db_failover','db_id','db_name','dbid','dbidexec','deadlock_priority','default_database','default_logon_domain',
+						'definition','delayed_durability','deny','description','desired_state','dtc_support','durability','enabled','encoding','encrypted','encrypted_value','encryption_type','endpoint','endpoint_url','entity_id','entity_name','error_message','error_number','error_severity',
+						'error_state','event','expiredate','expiry_date','failover_mode','failure_condition_level','federated_service_account','fetch_status','field_terminator','file_id','file_name','filegroup_id','filename','first','flush_interval_seconds','format_type','guid','hash',
+						'header_limit','health_check_timeout','high','host_name','identity','ignore_dup_key','index','interval_length_minutes','key_guid','key_id','key_name','key_path','key_store_provider_name','label','langid','language','length','level','lifetime','location',
+						'lock_escalation','lock_timeout','login_type','low','max_duration','max_outstanding_io_per_volume','max_plans_per_query','max_storage_size_mb','message','messages','minutes','mirror_address','month','monthname','name','namespace','object_id','object_name','objid',
+						'or','order','parameters','partition_id','password','path','permission_name','permission_set','Permissions','platform','population','port','precision','predicate','priority','procedure_name','program_name','provider','quarter','query_capture_mode','quoted_identifier',
+						'rank','read','read_only_routing_url','reject_sample_value','reject_type','reject_value','remote_service_name','replace','required_synchronized_secondaries_to_commit','resource_manager_location','retention_period','role','root','row','row_terminator','rowcount','rows',
+						'schema_id','schema_name','secondary_type','seeding_mode','sequence','serde_method','server','service_name','session_id','session_timeout','setopts','shard_map_name','sid','site','size_based_cleanup_mode','source','sql','sql_handle','ssl_port','start_date','state',
+						'statement','status','string_delimiter','subject','system','target','text','TextPtr','timeout','timestamp','trim','type','type_desc','type_name','uid','url','use_type_default','user','user_id','validation','value','version','weekday','weight','year'
+					) OR x.[name] LIKE '%[^a-z0-9_#]%' OR x.[name] LIKE '[0-9]%', QUOTENAME(x.[name]), x.[name])
 			) y
 		WHERE x.is_hidden = 0
 	) x
