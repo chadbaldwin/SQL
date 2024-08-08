d3988b9283a6402b9f62794d608804c0dd146cad
Chad Baldwin <chad@chadbaldwin.net>
2024-08-08 15:52:14 -0400

    Update dbo.uf_DateCalc.UserDefinedFunction.sql

--- dbo.uf_DateCalc.UserDefinedFunction.sql
+++ dbo.uf_DateCalc.UserDefinedFunction.sql
@@ -25,36 +25,36 @@ BEGIN;
     SELECT Code                 = x.Code
         , [Label]               = CONVERT(varchar(100), x.[Period])
         , BeginDate             = CONVERT(datetime2, x.BeginDate)
-        , EndDateInclusiveDT    = DATEADD(MILLISECOND, -3, DATEADD(DAY, 1, CONVERT(datetime, x.EndDate)))
+        , EndDateInclusiveDT    = DATEADD(MILLISECOND,  -3, DATEADD(DAY, 1, CONVERT(datetime , x.EndDate)))
         , EndDateInclusiveDT2   = DATEADD(NANOSECOND, -100, DATEADD(DAY, 1, CONVERT(datetime2, x.EndDate))) -- +1 day -1 tick
         , EndDateExclusive      = DATEADD(DAY, 1, CONVERT(datetime2, x.EndDate))
     FROM cte_Dates t
         CROSS APPLY (
-            VALUES    ('CD'     , 'Current Day'                     , @SeedDate                         , @SeedDate                                         )
-                    , ('CM'     , 'Current Month'                   , DATEADD(mm, t.DiffMM, 0)          , DATEADD(dd, -1, DATEADD(mm, t.DiffMM + 1, 0))     )
-                    , ('CMTD'   , 'Current Month To Date'           , DATEADD(mm, t.DiffMM, 0)          , @SeedDate                                         )
-                    , ('CQ'     , 'Current Quarter'                 , DATEADD(qq, t.DiffQQ, 0)          , DATEADD(dd, -1, DATEADD(qq, t.DiffQQ + 1, 0))     )
-                    , ('CQTD'   , 'Current Quarter to Date'         , DATEADD(qq, t.DiffQQ, 0)          , @SeedDate                                         )
-                    , ('CY'     , 'Current Year'                    , DATEADD(yy, t.DiffYY, 0)          , DATEADD(dd, -1, DATEADD(yy, t.DiffYY + 1, 0))     )
-                    , ('CYTD'   , 'Current Year To Date'            , DATEADD(yy, t.DiffYY, 0)          , @SeedDate                                         )
+            VALUES    ('CD'     , 'Current Day'                     , @SeedDate                         , @SeedDate                                     )
+                    , ('CM'     , 'Current Month'                   , DATEADD(mm, t.DiffMM, 0)          , DATEADD(dd, -1, DATEADD(mm, t.DiffMM + 1, 0)) )
+                    , ('CMTD'   , 'Current Month To Date'           , DATEADD(mm, t.DiffMM, 0)          , @SeedDate                                     )
+                    , ('CQ'     , 'Current Quarter'                 , DATEADD(qq, t.DiffQQ, 0)          , DATEADD(dd, -1, DATEADD(qq, t.DiffQQ + 1, 0)) )
+                    , ('CQTD'   , 'Current Quarter to Date'         , DATEADD(qq, t.DiffQQ, 0)          , @SeedDate                                     )
+                    , ('CY'     , 'Current Year'                    , DATEADD(yy, t.DiffYY, 0)          , DATEADD(dd, -1, DATEADD(yy, t.DiffYY + 1, 0)) )
+                    , ('CYTD'   , 'Current Year To Date'            , DATEADD(yy, t.DiffYY, 0)          , @SeedDate                                     )
 
-                    , ('PD'     , 'Previous Day'                    , DATEADD(dd, -1, @SeedDate)            , DATEADD(dd, -1, @SeedDate)                    )
-            --      , ('PW'     , 'Previous Week'                   , DATEADD(wk, y.DiffWK - 1, 0)      , DATEADD(dd, -1, DATEADD(wk, y.DiffWK, 0))         ) -- Removed temporarily - for some reason defaults to Monday as first day of week, despite DATEFIRST setting
-                    , ('PM'     , 'Previous Month'                  , DATEADD(mm, t.DiffMM - 1, 0)      , DATEADD(dd, -1, DATEADD(mm, t.DiffMM, 0))         )
-                    , ('PMTD'   , 'Previous Month to Date'          , DATEADD(mm, t.DiffMM - 1, 0)      , DATEADD(mm, -1, @SeedDate)                        )
-                    , ('PQ'     , 'Previous Quarter'                , DATEADD(qq, t.DiffQQ - 1, 0)      , DATEADD(dd, -1, DATEADD(qq, t.DiffQQ, 0))         )
-                    , ('PQTD'   , 'Previous Quarter to Date'        , DATEADD(qq, t.DiffQQ - 1, 0)      , DATEADD(qq, -1, @SeedDate)                        )
-                    , ('PYQ'    , 'Previous Year Quarter'           , DATEADD(qq, t.DiffQQ - 4, 0)      , DATEADD(dd, -1, DATEADD(qq, t.DiffQQ - 3, 0))     )
-                    , ('PYQTD'  , 'Previous Year Quarter to Date'   , DATEADD(qq, t.DiffQQ - 4, 0)      , DATEADD(yy, -1, @SeedDate)                        )
-                    , ('PY'     , 'Previous Year'                   , DATEADD(yy, t.DiffYY - 1, 0)      , DATEADD(dd, -1, DATEADD(yy, t.DiffYY, 0))         )
-                    , ('PYTD'   , 'Previous Year to Date'           , DATEADD(yy, t.DiffYY - 1, 0)      , DATEADD(yy, -1, @SeedDate)                        )
+                    , ('PD'     , 'Previous Day'                    , DATEADD(dd, -1, @SeedDate)        , DATEADD(dd, -1, @SeedDate)                    )
+            --      , ('PW'     , 'Previous Week'                   , DATEADD(wk, y.DiffWK - 1, 0)      , DATEADD(dd, -1, DATEADD(wk, y.DiffWK, 0))     ) -- Removed temporarily - for some reason defaults to Monday as first day of week, despite DATEFIRST setting
+                    , ('PM'     , 'Previous Month'                  , DATEADD(mm, t.DiffMM - 1, 0)      , DATEADD(dd, -1, DATEADD(mm, t.DiffMM, 0))     )
+                    , ('PMTD'   , 'Previous Month to Date'          , DATEADD(mm, t.DiffMM - 1, 0)      , DATEADD(mm, -1, @SeedDate)                    )
+                    , ('PQ'     , 'Previous Quarter'                , DATEADD(qq, t.DiffQQ - 1, 0)      , DATEADD(dd, -1, DATEADD(qq, t.DiffQQ, 0))     )
+                    , ('PQTD'   , 'Previous Quarter to Date'        , DATEADD(qq, t.DiffQQ - 1, 0)      , DATEADD(qq, -1, @SeedDate)                    )
+                    , ('PYQ'    , 'Previous Year Quarter'           , DATEADD(qq, t.DiffQQ - 4, 0)      , DATEADD(dd, -1, DATEADD(qq, t.DiffQQ - 3, 0)) )
+                    , ('PYQTD'  , 'Previous Year Quarter to Date'   , DATEADD(qq, t.DiffQQ - 4, 0)      , DATEADD(yy, -1, @SeedDate)                    )
+                    , ('PY'     , 'Previous Year'                   , DATEADD(yy, t.DiffYY - 1, 0)      , DATEADD(dd, -1, DATEADD(yy, t.DiffYY, 0))     )
+                    , ('PYTD'   , 'Previous Year to Date'           , DATEADD(yy, t.DiffYY - 1, 0)      , DATEADD(yy, -1, @SeedDate)                    )
         ) x(Code, [Period], BeginDate, EndDate)
     WHERE x.Code = @DateCode OR @DateCode IS NULL
     UNION
     SELECT Code                 = UPPER(@DateCode)
         , [Label]               = CONCAT_WS(' ', CASE LEFT(@DateCode, 1) WHEN 'L' THEN 'Last' WHEN 'P' THEN 'Previous' ELSE NULL END, t.[Value], 'days')
         , BeginDate             = CONVERT(datetime2, x.BeginDate)
-        , EndDateInclusiveDT    = DATEADD(MILLISECOND, -3, DATEADD(DAY, 1, CONVERT(datetime, x.EndDate)))
+        , EndDateInclusiveDT    = DATEADD(MILLISECOND,  -3, DATEADD(DAY, 1, CONVERT(datetime , x.EndDate)))
         , EndDateInclusiveDT2   = DATEADD(NANOSECOND, -100, DATEADD(DAY, 1, CONVERT(datetime2, x.EndDate))) -- +1 day -1 tick
         , EndDateExclusive      = DATEADD(DAY, 1, CONVERT(datetime2, x.EndDate))
     FROM (
